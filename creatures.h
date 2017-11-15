fe94db3cb6e5157a462017fbef1b317af99efdf5
nxtstep101 <nxtstep101@gmail.com>
2017-11-14 23:13:34 -0600

    Create creatures.h

---
+++ creatures.h
@@ -0,0 +1,29 @@
+#ifndef CREATURES_H
+#define CREATURES_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+struct Cost {
+	double careHours, costPerHour, foodCost, materialsCost;
+};
+
+struct Creatures {
+  string creatureName, creatureDescription, creatureLocation;
+	double avgLength, avgHeight;
+	bool creatureThreat;
+	Cost creatureCost;
+};
+
+int enterCreatures(int, Creatures[]);
+void printCreatures(int, Creatures[]);
+void printStatistics(int, Creatures[]);
+void saveCreatureToFile(int, Creatures[]);
+float convertToFloat(string);
+
+#endif
